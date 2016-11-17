18c858f13eebfff66c56c7e96665db26fe9ea2a0
anybalrocha <anybalrocha@hotmail.com>
2016-11-17 02:08:07 -0200

    Inclusão de projeto.

---
+++ README.md
@@ -0,0 +1,5 @@
+# ASP.NET MVC - Customizando DisplayFor e EditFor
+
+Projeto destinado aos desenvolvedores ASP.NET MVC, com o objetivo de apresentar os recursos de customização do DisplayFor e do EditFor. Onde você desenvolve seu componente e consegue aplicar este nas suas views, atreladas ao tipo da sua propriedade no seu Model ou ViewModel desta view; aproveitando o código e centralizando a lógica de apresentação deste tipo de dado. 
+
+Esta abordagem concede ao desenvolvedor maior produtividade no desenvolvimento de sistemas e facilita a manutenção do código relacionado a este ponto no seu projeto, além de conceder maior liberdade quanto a apresentação e comportamento do tipo de dado nas suas views.
---
+++ WebApplication1/Controllers/PessoaController.cs
@@ -0,0 +1,101 @@
+﻿using Service.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PessoaController : Controller
+    {
+        //
+        // GET: /Pessoa/
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "Minha primeira View";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PessoaViewModel model, FormCollection formCollection)
+        {
+            ModelState.Remove("Codigo");
+
+            var dia = Convert.ToInt32(formCollection["DataNascimento.days"]);
+            var mes = Convert.ToInt32(formCollection["DataNascimento.months"]);
+            var ano = Convert.ToInt32(formCollection["DataNascimento.years"]);
+
+            DateTime data = new DateTime(ano, mes, dia);
+            model.DataNascimento = data;
+
+            ModelState["DataNascimento"].Errors.Clear();
+
+            if (ModelState.IsValid)
+            {
+                if (model.Captcha == "123#$")
+                {
+                    var p = AutoMapper.Mapper.Map<PessoaViewModel, Pessoa>(model);
+
+                    PessoaService srv = new PessoaService();
+                    srv.Salvar(p);
+
+                    return View("List", srv.Listar());
+                }
+                return View(model);
+            }
+            else
+                return View(model);
+        }
+
+        public ActionResult List()
+        {
+            PessoaService srv = new PessoaService();
+            return View(srv.Listar());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var srv = new PessoaService();
+            var p = srv.Obter(id);
+
+            var pVM = AutoMapper.Mapper.Map<Pessoa, PessoaViewModel>(p);
+
+            if((pVM.Codigo % 2) == 0)
+                pVM.Ativo = true;
+
+            return View("Create", pVM);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PessoaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var p = AutoMapper.Mapper.Map<PessoaViewModel, Pessoa>(model);
+
+                PessoaService srv = new PessoaService();
+                srv.Salvar(p);
+
+                return View("List", srv.Listar());
+            }
+            else
+                return View("Create", model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            PessoaService srv = new PessoaService();
+            srv.Deletar(id);
+
+            return View("List", srv.Listar());
+        }
+	}
+}
